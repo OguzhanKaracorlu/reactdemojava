09fb940dd44d209325fbfa2d51466f5080ba2073
oguzhan.karacorlu <oguzhankaracorlu@gmail.com>
2023-04-10 10:39:10 +0300

    Rename Start Class

--- src/main/java/com/karacorlu/oguzhan/reactdemojava/ReactDemoJavaApplication.java
+++ src/main/java/com/karacorlu/oguzhan/reactdemojava/ReactDemoJavaApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ReactdemojavaApplication {
+public class ReactDemoJavaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ReactdemojavaApplication.class, args);
+		SpringApplication.run(ReactDemoJavaApplication.class, args);
 	}
 
 }
