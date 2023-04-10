149041d9248643f96fe5b52a516eaafff96a24fc
oguzhan.karacorlu <oguzhankaracorlu@gmail.com>
2023-04-10 10:31:06 +0300

    Init project

---
+++ src/main/java/com/karacorlu/oguzhan/reactdemojava/ReactdemojavaApplication.java
@@ -0,0 +1,13 @@
+package com.karacorlu.oguzhan.reactdemojava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactdemojavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactdemojavaApplication.class, args);
+	}
+
+}
